f7936d8fe18266ea2683ba5648a01f851daa9e6f
Stanislav Kiselev <stif@tut.by>
2021-01-15 21:49:50 +0300

    feat: add interactive menu

---
+++ script.js
@@ -0,0 +1,24 @@
+// const MENU = document.getElementById('menu');
+
+// MENU.addEventListener('click', (event) => {
+//   MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+//   event.target.classList.add('active');
+// });
+
+document.addEventListener('scroll', onScroll);
+
+function onScroll(event) {
+  const curPos = window.scrollY;
+  const divs = document.querySelectorAll('#body>*');
+  const links = document.querySelectorAll('#menu a');
+  divs.forEach((el) => {
+    if (el.offsetTop <= curPos && (el.offsetTop + el.offsetHeight) > curPos) {
+      links.forEach((a) => {
+        a.classList.remove('active');
+        if (el.getAttribute('id') === a.getAttribute('href').substring(1)) {
+          a.classList.add('active');
+        }
+      })
+    }
+  });
+}
